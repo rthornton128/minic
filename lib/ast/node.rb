7cb08a51f48ba0f9daf0482b3100ae8bd2e5c011
= <rob@thethorntons.ca>
2025-01-11 17:30:42 -0500

    Node should be part of the AST
    Node was not properly nested within its parent's constants.

--- lib/ast/node.rb
+++ lib/ast/node.rb
@@ -1,24 +1,28 @@
 # typed: true
 # frozen_string_literal: true
 
-class Node
-  extend T::Helpers
+module Minic
+  class AbstractSyntaxTree
+    class Node
+      extend T::Helpers
 
-  abstract!
+      abstract!
 
-  sig { returns(String) }
-  attr_reader :literal
+      sig { returns(String) }
+      attr_reader :literal
 
-  sig { returns(Integer) }
-  attr_reader :length, :offset
+      sig { returns(Integer) }
+      attr_reader :length, :offset
 
-  sig { params(literal: String, offset: Integer).void }
-  def initialize(literal:, offset:)
-    @length = T.let(literal.size, Integer)
-    @literal = literal
-    @offset = offset
+      sig { params(literal: String, offset: Integer).void }
+      def initialize(literal:, offset:)
+        @length = T.let(literal.size, Integer)
+        @literal = literal
+        @offset = offset
+      end
+
+      sig { abstract.params(block: T.proc.params(node: Node).void).void }
+      def walk(&block); end
+    end
   end
-
-  sig { abstract.params(block: T.proc.params(node: Node).void).void }
-  def walk(&block); end
 end
