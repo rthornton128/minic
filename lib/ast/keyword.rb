08aa1348ac7e713a0694a8d1f52904234ae7d9d6
= <rob@thethorntons.ca>
2025-01-11 19:14:45 -0500

    Remove unneccessary walk method
    The walk method is implemented by the parent class.

--- lib/ast/keyword.rb
+++ lib/ast/keyword.rb
@@ -4,8 +4,6 @@
 module Minic
   class AbstractSyntaxTree
     class Keyword < Identifier
-      sig { override.params(block: T.proc.params(node: Node).void).void }
-      def walk(&block); end
     end
   end
 end
