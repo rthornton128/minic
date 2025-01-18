590f473b1387d6348ce0abd2c604b68d53762856
= <rob@thethorntons.ca>
2025-01-18 11:16:09 -0500

    Type check basic var_decl
    Type checks variable declarations against assignment of literals.

--- lib/semantic_analyzer.rb
+++ lib/semantic_analyzer.rb
@@ -1,6 +1,10 @@
 # typed: true
 # frozen_string_literal: true
 
+require_relative "semantic_analyzer/type"
+require_relative "semantic_analyzer/basic_type"
+require_relative "semantic_analyzer/scope"
+
 module Minic
   class SemanticAnalyzer
     class Error < ::Minic::Error; end
@@ -11,17 +15,63 @@ module Minic
     end
 
     sig { void }
-    def analyze
-      analyze_program(program: @ast.program)
+    def check
+      program = @ast.program
+      scope = Scope.new(parent: nil)
+
+      program.declarations.each do |declaration|
+        case declaration
+        when AbstractSyntaxTree::VariableDeclaration
+          check_variable_decl(var_decl: declaration, scope:)
+        when AbstractSyntaxTree::FunctionDeclaration
+          check_function_decl(func_decl: declaration, scope:)
+        else
+          T.absurd(declaration)
+        end
+      end
     end
 
-    sig { params(program: AbstractSyntaxTree::Program).void }
-    def analyze_program(program:)
+    private
+
+    sig { params(func_decl: T.untyped, scope: T.untyped).returns(T.untyped) }
+    def check_function_decl(func_decl:, scope:)
+      # No op
+    end
+
+    sig { params(var_decl: AbstractSyntaxTree::VariableDeclaration, scope: Scope).void }
+    def check_variable_decl(var_decl:, scope:)
+      var_type = type_of(node: var_decl.type, scope:)
+      scope[var_decl.identifier.literal] = var_type
+
+      unless var_decl.assignment.nil?
+        expr_type = type_of(node: T.must(var_decl.assignment), scope:)
+        assert_types(var_type, expr_type)
+      end
+    end
+
+    sig { params(a: Type, b: Type).void }
+    def assert_types(a, b)
       raise Error.new(
-        "no entry point; program can't be empty",
-        program.literal,
-        program.offset,
-      ) if program.declarations.empty?
+        "type missmatch #{a}, #{b}",
+        "",
+        b.offset,
+      ) if a != b
+    end
+
+    sig { params(node: AbstractSyntaxTree::Node, scope: Scope).returns(Type) }
+    def type_of(node:, scope:)
+      case node
+      when AbstractSyntaxTree::BooleanLiteral
+        BasicType.new(name: "bool", offset: node.offset)
+      when AbstractSyntaxTree::DoubleLiteral
+        BasicType.new(name: "double", offset: node.offset)
+      when AbstractSyntaxTree::StringLiteral
+        BasicType.new(name: "string", offset: node.offset)
+      when AbstractSyntaxTree::IntegerLiteral
+        BasicType.new(name: "int", offset: node.offset)
+      else
+        BasicType.new(name: node.literal, offset: node.offset)
+      end
     end
   end
 end
--- test/semantic_analyzer_test.rb
+++ test/semantic_analyzer_test.rb
@@ -5,15 +5,76 @@ require "test_helper"
 
 module Minic
   class SemanticAnalyzerTest < TestCase
-    test "empty program is raises error" do
-      file = FileSet::File.new(body: "")
+    test "boolean literal assigned to boolean variable does not raise error" do
+      file = FileSet::File.new(body: "bool b = true; }")
       lexer = Lexer.new(file:)
       parser = Parser.new(lexer:)
-      analyzer = SemanticAnalyzer.new(ast: parser.parse)
+      ast = parser.parse
 
-      assert_raises(Error) do
-        analyzer.analyze
-      end
+      SemanticAnalyzer.new(ast:).check
+    end
+
+    test "boolean literal assigned to double variable does not raise error" do
+      file = FileSet::File.new(body: "double b = true; }")
+      lexer = Lexer.new(file:)
+      parser = Parser.new(lexer:)
+      ast = parser.parse
+
+      assert_raises(SemanticAnalyzer::Error) { SemanticAnalyzer.new(ast:).check }
+    end
+
+    test "double literal assigned to double variable does not raise error" do
+      file = FileSet::File.new(body: "double d = 0.0; }")
+      lexer = Lexer.new(file:)
+      parser = Parser.new(lexer:)
+      ast = parser.parse
+
+      SemanticAnalyzer.new(ast:).check
+    end
+
+    test "double literal assigned to boolean variable does not raise error" do
+      file = FileSet::File.new(body: "double d = true; }")
+      lexer = Lexer.new(file:)
+      parser = Parser.new(lexer:)
+      ast = parser.parse
+
+      assert_raises(SemanticAnalyzer::Error) { SemanticAnalyzer.new(ast:).check }
+    end
+
+    test "integer literal assigned to integer variable does not raise error" do
+      file = FileSet::File.new(body: "int i = 1; }")
+      lexer = Lexer.new(file:)
+      parser = Parser.new(lexer:)
+      ast = parser.parse
+
+      SemanticAnalyzer.new(ast:).check
+    end
+
+    test "integer literal assigned to boolean variable does not raise error" do
+      file = FileSet::File.new(body: "int i = true; }")
+      lexer = Lexer.new(file:)
+      parser = Parser.new(lexer:)
+      ast = parser.parse
+
+      assert_raises(SemanticAnalyzer::Error) { SemanticAnalyzer.new(ast:).check }
+    end
+
+    test "string literal assigned to string variable does not raise error" do
+      file = FileSet::File.new(body: 'string s = "str"; }')
+      lexer = Lexer.new(file:)
+      parser = Parser.new(lexer:)
+      ast = parser.parse
+
+      SemanticAnalyzer.new(ast:).check
+    end
+
+    test "string literal assigned to boolean variable does not raise error" do
+      file = FileSet::File.new(body: "string s = true; }")
+      lexer = Lexer.new(file:)
+      parser = Parser.new(lexer:)
+      ast = parser.parse
+
+      assert_raises(SemanticAnalyzer::Error) { SemanticAnalyzer.new(ast:).check }
     end
   end
 end
