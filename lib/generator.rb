712a31ed8c4b6965faf689a24f15cc71b22b4e86
= <rob@thethorntons.ca>
2025-01-22 19:52:51 -0500

    Generate most nodes
    Generate code for everything except function calls.

--- lib/generator.rb
+++ lib/generator.rb
@@ -17,10 +17,112 @@ module Minic
 
     private
 
+    sig { params(block: AbstractSyntaxTree::Block, parameters: T::Array[AbstractSyntaxTree::Parameter]).void }
+    def generate_block(block, parameters)
+      @out.puts("{")
+
+      # ensure parameters are given zero values
+      parameters.each do |parameter|
+        generate_identifier(parameter.identifier)
+        @out.write("=")
+        generate_zero_value(parameter.type.literal)
+        generate_terminal
+      end
+
+      block.statements.each { |statement| generate_statement(statement) }
+      @out.puts("}")
+    end
+
+    sig { params(expression: AbstractSyntaxTree::Expression).void }
+    def generate_expression(expression)
+      case expression
+      when AbstractSyntaxTree::BooleanLiteral,
+        AbstractSyntaxTree::DoubleLiteral,
+        AbstractSyntaxTree::Identifier,
+        AbstractSyntaxTree::IntegerLiteral,
+        AbstractSyntaxTree::StringLiteral
+        @out.write(expression.literal)
+      when AbstractSyntaxTree::BinaryExpression
+        generate_expression(expression.lhs)
+        @out.write(expression.literal)
+        generate_expression(expression.rhs)
+      when AbstractSyntaxTree::FunctionCall
+      when AbstractSyntaxTree::SubExpression
+        @out.write("(")
+        generate_expression(expression.expression)
+        @out.write(")")
+      when AbstractSyntaxTree::UnaryExpression
+        @out.write(expression.literal)
+        generate_expression(expression.rhs)
+      end
+    end
+
+    sig { params(function_decl: AbstractSyntaxTree::FunctionDeclaration).void }
+    def generate_function_declaration(function_decl)
+      generate_type(function_decl.type)
+      generate_space
+      generate_identifier(function_decl.identifier)
+      generate_parameter_list(function_decl.parameter_list)
+      generate_terminal
+    end
+
+    sig { params(function_decl: AbstractSyntaxTree::FunctionDeclaration).void }
+    def generate_function_implementation(function_decl)
+      generate_type(function_decl.type)
+      generate_space
+      generate_identifier(function_decl.identifier)
+      generate_parameter_list(function_decl.parameter_list)
+      generate_block(function_decl.block, function_decl.parameter_list.parameters)
+    end
+
+    sig { params(identifier: AbstractSyntaxTree::Identifier).void }
+    def generate_identifier(identifier)
+      @out.write(identifier.literal)
+    end
+
+    sig { params(if_statement: AbstractSyntaxTree::IfStatement).void }
+    def generate_if_statement(if_statement)
+      @out.write("if")
+      @out.write("(")
+      generate_expression(if_statement.conditional)
+      @out.write(")")
+      generate_block(if_statement.then_block, [])
+
+      unless if_statement.else_block.nil?
+        @out.write("else")
+        generate_block(if_statement.then_block, [])
+      end
+    end
+
+    sig { params(declaration: AbstractSyntaxTree::Declaration).void }
+    def generate_implementation(declaration)
+      case declaration
+      when AbstractSyntaxTree::FunctionDeclaration
+        generate_function_implementation(declaration)
+      when AbstractSyntaxTree::VariableDeclaration
+        generate_variable_decl(declaration)
+      end
+    end
+
+    sig { params(parameter_list: AbstractSyntaxTree::ParameterList).void }
+    def generate_parameter_list(parameter_list)
+      @out.write("(")
+
+      parameter_list.parameters.each do |parameter|
+        generate_type(parameter.type)
+        generate_space
+        generate_identifier(parameter.identifier)
+        @out.write(",") unless parameter == parameter_list.parameters.last
+      end
+      @out.write(")")
+    end
+
     sig { params(program: AbstractSyntaxTree::Program).void }
     def generate_program(program)
       @out.puts('#include "minic.h"')
-      generate_declarations
+      program.declarations.each do |declaration|
+        generate_function_declaration(declaration) if declaration.is_a?(AbstractSyntaxTree::FunctionDeclaration)
+      end
 
       program.declarations.each do |declaration|
         generate_implementation(declaration)
@@ -28,20 +130,44 @@ module Minic
     end
 
     sig { void }
-    def generate_declarations
+    def generate_space
+      @out.write(" ")
     end
 
-    sig { params(func_type: SemanticAnalyzer::FunctionType).void }
-    def generate_function_declaration(func_type)
-    end
-
-    sig { params(declaration: AbstractSyntaxTree::Declaration).void }
-    def generate_implementation(declaration)
-      case declaration
-      when AbstractSyntaxTree::FunctionDeclaration
+    sig { params(statement: AbstractSyntaxTree::Statement).void }
+    def generate_statement(statement)
+      case statement
+      when AbstractSyntaxTree::AssignmentStatement
+        generate_identifier(statement.lhs)
+        @out.write("=")
+        generate_expression(T.must(statement.rhs))
+      when AbstractSyntaxTree::FunctionCall
+      when AbstractSyntaxTree::IfStatement
+        generate_if_statement(statement)
+      when AbstractSyntaxTree::ReturnStatement
+        @out.write("return")
+        generate_space
+        generate_expression(T.must(statement.expression)) unless statement.expression.nil?
       when AbstractSyntaxTree::VariableDeclaration
-        generate_variable_decl(declaration)
+      when AbstractSyntaxTree::WhileStatement
+        generate_while_statement(statement)
+      else
+        T.absurd(statement)
       end
+
+      generate_terminal
+    end
+
+    sig { void }
+    def generate_terminal
+      @out.puts(";")
+    end
+
+    sig { params(type: AbstractSyntaxTree::Identifier).void }
+    def generate_type(type)
+      @out.write("char *)") if type.literal == "string"
+
+      @out.write(type.literal)
     end
 
     sig { params(declaration: AbstractSyntaxTree::VariableDeclaration).void }
@@ -60,14 +186,19 @@ module Minic
       if declaration.assignment.nil?
         generate_zero_value(type_name)
       else
-        generate_assignment(T.must(declaration.assignment))
+        generate_expression(T.must(declaration.assignment))
       end
 
-      @out.puts(";")
+      generate_terminal
     end
 
-    sig { params(assignment: AbstractSyntaxTree::Expression).void }
-    def generate_assignment(assignment)
+    sig { params(while_statement: AbstractSyntaxTree::WhileStatement).void }
+    def generate_while_statement(while_statement)
+      @out.write("while")
+      @out.write("(")
+      generate_expression(while_statement.conditional)
+      @out.write(")")
+      generate_block(while_statement.block, [])
     end
 
     sig { params(type_name: String).void }
