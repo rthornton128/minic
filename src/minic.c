513e14caa8f9664baeece9f611e2ceccfe5333c1
= <rob@thethorntons.ca>
2025-01-26 10:46:44 -0500

    Provide print method in stdlib
    Exposes the C printf function to the standard library. Since Minic doesn't support variable arguments, generic types, proper strings, or type casting, a built-in function must be provided.
    
    An include guard was added to the minic.h header.

--- src/minic.c
+++ src/minic.c
@@ -1,1 +1,12 @@
 #include "minic.h"
+
+#include <stdarg.h>
+#include <stdio.h>
+
+void print(char *format, ...)
+{
+  va_list args;
+  va_start(args, format);
+  vprintf(format, args);
+  va_end(args);
+}
